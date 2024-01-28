6457483f945e89f9d7888ae267db43dc077325c3
FutureLizard205 <miguelbarraalmeida@gmail.com>
2024-01-28 01:26:50 +0000

    Organize and structure code

--- src/main.c
+++ src/main.c
@@ -1,62 +1,87 @@
+#include <SDL2/SDL_render.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
-#include <SDL.h>
-#include <SDL_image.h>
-#include "globalConstants.h"
+#include <string.h>
+#include "sdlInclude.h"
+
+#include "globalStructs.h"
+
+#define macro_error(condition, message) \
+  if (condition) { \
+    SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", message, NULL); \
+    return EXIT_FAILURE; \
+  }
+
 
 int main(int argc, char *argv[]) {
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        printf("Failed to initialize SDL2.\n");
-        return EXIT_FAILURE;
+  
+  // Global Data Initialization
+
+  globalData_t globalData;
+
+  globalData.app.windowHSize = 640;
+  globalData.app.windowVSize = 480;
+  strcpy(globalData.app.windowTitle, "WarpWiggle");
+  globalData.app.fpsLimit = 0;
+
+
+  // SDL2 Initialization
+
+  macro_error(SDL_Init(SDL_INIT_VIDEO) < 0, "Failed to initialize SDL2.");
+
+  globalData.app.window = SDL_CreateWindow(globalData.app.windowTitle,
+                                        SDL_WINDOWPOS_CENTERED,
+                                        SDL_WINDOWPOS_CENTERED,
+                                        globalData.app.windowHSize,
+                                        globalData.app.windowVSize,
+                                        0);
+  
+  macro_error(!globalData.app.window, "Failed to create window.");
+
+  globalData.app.renderer = SDL_CreateRenderer(globalData.app.window, -1, SDL_RENDERER_PRESENTVSYNC);
+
+  macro_error(!globalData.app.renderer, "Failed to create renderer.");
+ 
+
+  // Game Loop
+  
+  SDL_Event event;
+  const double fps_limit_ms = 1000 / (double) globalData.app.fpsLimit;
+  uint64_t perfCount_cycle_start;
+  uint64_t perfCount_cycle_end;
+  double time_cycle;
+  while (1) {
+
+    // Get perfomance counter (for fps calculation)
+    perfCount_cycle_start = SDL_GetPerformanceCounter();
+    
+
+    // Manage events
+    while (SDL_PollEvent(&event)) {
+      switch (event.type) {
+         case SDL_QUIT:
+             SDL_Quit();
+             return EXIT_SUCCESS;
+      }
+    }
+    
+
+    // Render
+    SDL_SetRenderDrawColor(globalData.app.renderer, 130, 210, 250, SDL_ALPHA_OPAQUE);
+	  SDL_RenderClear(globalData.app.renderer);
+    SDL_RenderPresent(globalData.app.renderer);
+
+
+    // FPS Limiter
+    if (globalData.app.fpsLimit != 0) {
+      perfCount_cycle_end = SDL_GetPerformanceCounter();
+      time_cycle = 1000 * (perfCount_cycle_end - perfCount_cycle_start) / (double) SDL_GetPerformanceFrequency();
+      if (time_cycle < fps_limit_ms) {
+        SDL_Delay(fps_limit_ms - time_cycle);
+      }
     }
 
-    SDL_Window *window = SDL_CreateWindow(WINDOW_TITLE,
-                                          SDL_WINDOWPOS_CENTERED,
-                                          SDL_WINDOWPOS_CENTERED,
-                                          WINDOW_HORIZONTAL_SIZE,
-                                          WINDOW_VERTICAL_SIZE,
-                                          SDL_WINDOW_BORDERLESS);
+  }
 
-    if (!window) {
-        printf("Failed to create window.\n");
-        return EXIT_FAILURE;
-    }
-
-    SDL_Surface *window_surface = SDL_GetWindowSurface(window);
-
-    if (!window_surface) {
-        printf("Failed to get the surface from the window\n");
-        return EXIT_FAILURE;
-    }
-
-    SDL_Event event;
-    const double fps_limit_ms = 1000 / (double) FPS_LIMIT;
-    uint64_t perfCount_cycle_start;
-    uint64_t perfCount_cycle_end;
-    double time_cycle;
-    for (int i = 0; i < 200; i++) {
-        perfCount_cycle_start = SDL_GetPerformanceCounter();
-
-
-        while (SDL_PollEvent(&event)) {
-            switch (event.type) {
-                case SDL_QUIT:
-                    SDL_Quit();
-                    return 0;
-            }
-        }
-
-        SDL_UpdateWindowSurface(window);
-
-
-        perfCount_cycle_end = SDL_GetPerformanceCounter();
-        time_cycle = 1000 * (perfCount_cycle_end - perfCount_cycle_start) / (double) SDL_GetPerformanceFrequency();
-        if (time_cycle < fps_limit_ms) {
-            SDL_Delay(fps_limit_ms - time_cycle);
-        }
-
-    }
-
-    return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
